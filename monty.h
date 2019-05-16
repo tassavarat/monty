1e8a6f510df547d9eb02329defc11f69144aef7e
vdphan <673@holbertonschool.com>
2019-05-16 19:13:48 +0000

    create div prototype

--- monty.h
+++ monty.h
@@ -62,6 +62,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void divide(stack_t **stack, unsigned int line_number);
 
 /* monty_check */
 void error_handle(stack_t **stack, unsigned int line_number, int error_type);
