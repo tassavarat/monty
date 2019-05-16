0fa16462badd757a5f7d0ad237a96b5a16427fe4
vdphan <673@holbertonschool.com>
2019-05-16 00:47:20 +0000

    add pint prototype

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+void pint(stack_t **stack, unsigned int line_number);
 
 /* monty_check */
 void error_handle(stack_t **stack, unsigned int line_number, int error_type);
